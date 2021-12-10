515f50a0389de4f6d5c82b598d2dd0eb88b2e640
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-12-10 04:46:29 -0500

    starting work on landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/landingPage.css';
+
+export default function LandingPage() {
+  return <div className="landing-page"></div>;
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/navBar.css';
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <div className="image"></div>
+        <div className="nav-links">
+          <ul>
+            <li>About</li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
