83a37430d667136f452b075f6a96e65368df538d
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-12-25 21:16:02 -0500

    backend frontend connection seems to be working

--- root/backend/server/index.js
+++ root/backend/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const randomArtist = require('./api/randomArtist');
 
 const PORT = process.env.PORT || 3001;
@@ -6,11 +7,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', (req, res) =>
-  res.sendFile('/Users/rohan/muse-app/root/frontend/public/index.html')
-);
+app.use(express.static(path.join(__dirname, '../../', 'frontend/build')));
 
 app.use('/api', randomArtist);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../', 'frontend/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
