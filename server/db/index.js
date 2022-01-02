1e6acd73ae4ddec70245954005c5f73d1437c302
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-01-02 03:17:09 -0500

    heroku deployment unsucessful, will try again

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const pool = new Pool({
   user: 'rohan',
   password: 'Mastodon0d',
   database: 'musedb',
-  host: 'ec2-3-227-154-49.compute-1.amazonaws.com',
+  host: 'localhost',
   port: '5432',
 });
 
