8d6430594fe841be510274b15dbfc6ba2e14a438
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-01-02 00:20:30 -0500

    serving from build folder

--- root/backend/server/index.js
+++ root/backend/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 const randomArtists = require('./api/randomArtists');
 const searchByID = require('./api/searchByID');
 const getArtists = require('./api/getArtists');
@@ -10,16 +10,16 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, '../../', 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../../', 'frontend/build')));
 
 app.use('/api', getArtists);
 app.use('/api', randomArtists);
 app.use('/api', searchByID);
 app.use('/api', searchByImageID);
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../', 'frontend/build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../', 'frontend/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
